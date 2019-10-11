033b1e4c928a58e2d532ffb49b99f6b9aa8bb0f0
SimranMakhija7 <simran.makhija2@gmail.com>
2019-10-12 03:15:26 +0530

    components initialised and bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HelpComponent } from './help/help.component';
+import { MapComponent } from './home/map/map.component';
+import { ListComponent } from './home/list/list.component';
+import { ListItemComponent } from './home/list/list-item/list-item.component';
+import { DetailsComponent } from './home/details/details.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HelpComponent,
+    MapComponent,
+    ListComponent,
+    ListItemComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
